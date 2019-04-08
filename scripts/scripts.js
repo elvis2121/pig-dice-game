24bd33d5ea80fd2373ca564ecc7f7bfc974e9020
elvis2121 <elvisnjau@gmail.com>
2019-04-08 07:15:21 +0300

    js player constructor declaration and initialization

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+  
+  function Player(id,score,totalScore) {
+    this.id =id;
+    this.totalScore = 0;
+  }
+  player1 = new Player();
+  player2 = new Player();
